b4fef167cb11c38a377159faaa8a68a011458fc7
Rodrigo Assunção Veloso <rodrigo.aveloso@hotmail.com>
2022-03-11 20:21:22 -0300

    projeto criado
    criado projeto

---
+++ ichiraku-burguer/src/main/java/com/sete/ichirakuburguer/IchirakuBurguerApplication.java
@@ -0,0 +1,13 @@
+package com.sete.ichirakuburguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IchirakuBurguerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IchirakuBurguerApplication.class, args);
+	}
+
+}
