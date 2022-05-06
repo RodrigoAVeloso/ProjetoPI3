9656724a58570a833e245660f5b8b33eecd6ce0f
Rodrigo Assunção <Rodrigo.aveloso@hotmail.com>
2022-05-06 14:53:45 -0300

    Teste func
    teste func

--- src/main/java/br/com/projetopi3/ichirakuburguer/controller/ProdutoController.java
+++ src/main/java/br/com/projetopi3/ichirakuburguer/controller/ProdutoController.java
@@ -1,20 +1,22 @@
 package br.com.projetopi3.ichirakuburguer.controller;
 
 
-import br.com.projetopi3.ichirakuburguer.dto.produto.ProdutoDto;
-import br.com.projetopi3.ichirakuburguer.service.ProdutoService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import br.com.projetopi3.ichirakuburguer.dto.produto.ProdutoDto;
+import br.com.projetopi3.ichirakuburguer.service.ProdutoService;
 
 @Controller
+@RequestMapping(value = "/produtos")
 public class ProdutoController {
 
     @Autowired
