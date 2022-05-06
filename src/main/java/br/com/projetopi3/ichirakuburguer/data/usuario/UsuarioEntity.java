b89a6d36056b4eaf5441124381e20d7885880f57
cauebatistaa <82001514+cauebatistaa@users.noreply.github.com>
2022-05-06 14:53:22 -0300

    teste funcionario
    teste funcionario

--- src/main/java/br/com/projetopi3/ichirakuburguer/data/usuario/UsuarioEntity.java
+++ src/main/java/br/com/projetopi3/ichirakuburguer/data/usuario/UsuarioEntity.java
@@ -1,4 +1,26 @@
 package br.com.projetopi3.ichirakuburguer.data.usuario;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FUNCIONARIO")
 public class UsuarioEntity {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String nome;	
+	
+	@Column
+	private String email;
+	
+	
 }
