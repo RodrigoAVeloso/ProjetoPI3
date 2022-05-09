f5a4eb75d85a1b845afb76d624d912450220f7c7
cauebatistaa <82001514+cauebatistaa@users.noreply.github.com>
2022-05-09 16:37:30 -0300

    entidade venda comentada

--- src/main/java/br/com/projetopi3/ichirakuburguer/data/produtoVenda/ProdutoVendaEntity.java
+++ src/main/java/br/com/projetopi3/ichirakuburguer/data/produtoVenda/ProdutoVendaEntity.java
@@ -1,62 +1,62 @@
-package br.com.projetopi3.ichirakuburguer.data.produtoVenda;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-
-import br.com.projetopi3.ichirakuburguer.data.produto.ProdutoEstoqueEntity;
-
-@Entity
-public class ProdutoVendaEntity {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	@Column(nullable = false)
-	private Integer quantidade;
-	@Column(nullable = false)
-	private float preco;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "produto_id", referencedColumnName = "id")
-	ProdutoEstoqueEntity produtoEstoque;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Integer getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(Integer quantidade) {
-		this.quantidade = quantidade;
-	}
-
-	public float getPreco() {
-		return preco;
-	}
-
-	public void setPreco(float preco) {
-		this.preco = preco;
-	}
-
-	public ProdutoEstoqueEntity getProdutoEstoque() {
-		return produtoEstoque;
-	}
-
-	public void setProdutoEstoque(ProdutoEstoqueEntity produtoEstoque) {
-		this.produtoEstoque = produtoEstoque;
-	}
-	
-	
-}
+//package br.com.projetopi3.ichirakuburguer.data.produtoVenda;
+//
+//import javax.persistence.CascadeType;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.OneToOne;
+//
+//import br.com.projetopi3.ichirakuburguer.data.produto.ProdutoEstoqueEntity;
+//
+//@Entity
+//public class ProdutoVendaEntity {
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Integer id;
+//	@Column(nullable = false)
+//	private Integer quantidade;
+//	@Column(nullable = false)
+//	private float preco;
+//	
+//	@OneToOne(cascade = CascadeType.ALL)
+//	@JoinColumn(name = "produto_id", referencedColumnName = "id")
+//	ProdutoEstoqueEntity produtoEstoque;
+//
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public Integer getQuantidade() {
+//		return quantidade;
+//	}
+//
+//	public void setQuantidade(Integer quantidade) {
+//		this.quantidade = quantidade;
+//	}
+//
+//	public float getPreco() {
+//		return preco;
+//	}
+//
+//	public void setPreco(float preco) {
+//		this.preco = preco;
+//	}
+//
+//	public ProdutoEstoqueEntity getProdutoEstoque() {
+//		return produtoEstoque;
+//	}
+//
+//	public void setProdutoEstoque(ProdutoEstoqueEntity produtoEstoque) {
+//		this.produtoEstoque = produtoEstoque;
+//	}
+//	
+//	
+//}
